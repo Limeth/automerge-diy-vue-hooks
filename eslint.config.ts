f12f17afab6eb282a92ade390bba3415d626efd6
Jakub Hlusička <Limeth@users.noreply.github.com>
2025-06-25 21:21:54 +0200

    Format code

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,11 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginOxlint from 'eslint-plugin-oxlint'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { globalIgnores } from "eslint/config";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from "@vue/eslint-config-typescript";
+import pluginVue from "eslint-plugin-vue";
+import pluginOxlint from "eslint-plugin-oxlint";
+import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -10,17 +13,17 @@ import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(["**/dist/**", "**/dist-ssr/**", "**/coverage/**"]),
 
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
-  ...pluginOxlint.configs['flat/recommended'],
+  ...pluginOxlint.configs["flat/recommended"],
   skipFormatting,
 
   {
-    name: 'app/files-to-lint',
+    name: "app/files-to-lint",
     rules: {
       "@typescript-eslint/no-explicit-any": ["off"],
     },
   },
-)
+);
--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-import { type Ref, ref } from 'vue';
+import { type Ref, ref } from "vue";
 import * as A from "@automerge/automerge-repo";
 // import * as Cabbages from "cabbages";
 
-export const changeSubtree = Symbol('changeSubtree');
+export const changeSubtree = Symbol("changeSubtree");
 
 export type Incomplete<T> = {
-  [P in keyof T]?: T[P]
-}
+  [P in keyof T]?: T[P];
+};
 
 /// A type with read-only properties.
 export type Ro<T> = {
-  readonly [P in keyof T]: T[P] extends object ? Rop<T[P]> : T[P]
-}
+  readonly [P in keyof T]: T[P] extends object ? Rop<T[P]> : T[P];
+};
 
 export type Proxy<T> = {
-  [changeSubtree]: (changeSubtreeCallback: (subtree: T) => void) => void,
+  [changeSubtree]: (changeSubtreeCallback: (subtree: T) => void) => void;
 };
 
 /// `Rop` stands for Read-Only Proxy.
@@ -27,51 +27,55 @@ export type Rop<T> = Ro<T> & Proxy<T>;
 
 /// Creates a Vue-reactive proxy object of the a document, given a doc handle.
 export function makeReactive<T>(handle: A.DocHandle<T>): Ref<Rop<A.Doc<T>>> {
-  function makeProxy<U extends object>(obj: U, path: (string | number)[]): Rop<U> {
+  function makeProxy<U extends object>(
+    obj: U,
+    path: (string | number)[],
+  ): Rop<U> {
     let proxy;
-    
+
     // Include all regular properties in the proxy object, creating proxies
     // from any properties which hold an object.
-    if (Array.isArray(obj))
-    {
+    if (Array.isArray(obj)) {
       proxy = [] as Array<Rop<U>> & Incomplete<Proxy<T>>;
-      for (const [index, value] of obj.entries())
-      {
-        if (value !== null && typeof value === 'object')
-          proxy.push(makeProxy(value, [...path, index]))
-        else
-          proxy.push(value);
+      for (const [index, value] of obj.entries()) {
+        if (value !== null && typeof value === "object")
+          proxy.push(makeProxy(value, [...path, index]));
+        else proxy.push(value);
       }
-    }
-    else
-    {
+    } else {
       proxy = {} as Incomplete<Rop<U>>;
-      for (const key of Object.getOwnPropertyNames(obj) as Array<keyof U & string>)
-      {
+      for (const key of Object.getOwnPropertyNames(obj) as Array<
+        keyof U & string
+      >) {
         const value = obj[key];
-        if (value !== null && typeof value === 'object')
-          (proxy[key] as Rop<U[keyof U & string] & object>) = makeProxy(value, [...path, key]);
-        else
-          (proxy[key] as U[keyof U & string]) = value;
+        if (value !== null && typeof value === "object")
+          (proxy[key] as Rop<U[keyof U & string] & object>) = makeProxy(value, [
+            ...path,
+            key,
+          ]);
+        else (proxy[key] as U[keyof U & string]) = value;
       }
     }
 
     // Include all symbol properties as well, without modification.
-    for (const symbol of Object.getOwnPropertySymbols(obj) as Array<keyof U & symbol>)
+    for (const symbol of Object.getOwnPropertySymbols(obj) as Array<
+      keyof U & symbol
+    >)
       (proxy[symbol as keyof typeof proxy] as unknown) = obj[symbol];
 
     // Add custom methods as symbol properties, so that they don't interfere with regular usage.
-    (proxy[changeSubtree] as ((changeSubtreeCallback: (subtree: U) => void) => void)) = (changeSubtreeCallback: (subtree: U) => void) => {
+    (proxy[changeSubtree] as (
+      changeSubtreeCallback: (subtree: U) => void,
+    ) => void) = (changeSubtreeCallback: (subtree: U) => void) => {
       // `changeSubtree` operates on `A.DocHandle` directly.
       // The changes to the `A.DocHandle` are then applied to `newObj` via
       // the `on('change')` handler.
       handle.change((doc: A.Doc<T>) => {
         let subtree: any = doc;
-        for (const pathSegment of path)
-          subtree = subtree[pathSegment];
+        for (const pathSegment of path) subtree = subtree[pathSegment];
         changeSubtreeCallback(subtree as U);
-      })
-    }
+      });
+    };
 
     // FIXME: Slow? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/setPrototypeOf
     // Object.setPrototypeOf(proxy, Object.getPrototypeOf(obj));
@@ -81,8 +85,11 @@ export function makeReactive<T>(handle: A.DocHandle<T>): Ref<Rop<A.Doc<T>>> {
   const docProxy: Rop<A.Doc<T>> = makeProxy(handle.doc(), []);
   const docRef = ref<Rop<A.Doc<T>>>(docProxy) as Ref<Rop<A.Doc<T>>>;
 
-  handle.on('change', (payload) => {
-    console.debug("[automerge-diy-vue-hooks] Got 'change' event, applying patches.", payload);
+  handle.on("change", (payload) => {
+    console.debug(
+      "[automerge-diy-vue-hooks] Got 'change' event, applying patches.",
+      payload,
+    );
     // On any change, replace the whole document.
     docRef.value = makeProxy(handle.doc(), []);
 
